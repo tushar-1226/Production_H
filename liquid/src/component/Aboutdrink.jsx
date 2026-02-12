5ea91b10e9cf38edaa3584ed2151b6d9f3ea1bd9
harshit chakrawal <chakrawalharshit@gmail.com>
2026-02-12 21:19:22 +0530

    added img slider

--- liquid/src/component/Aboutdrink.jsx
+++ liquid/src/component/Aboutdrink.jsx
@@ -4,27 +4,27 @@ import { CampaImg } from '../assets/assets'
 
 const Aboutdrink = () => {
 
-  const [initialImg, setInitialImg] = useState("CampaImg[0]")
+  const [initialImg, setInitialImg] = useState(CampaImg[0])
 
-  const changeImg = (image) => {
-      setInitialImg === ""
+  const changeImg = (img) => {
+      setInitialImg(img)
   }
   return (
     <div className='flex flex-col'>
       <div className=" h-120 w-120 mb-5">
-        <img src={CampaImg[0]} className='' />
+        <img src={initialImg} className='' />
       </div>
       <div>
           <div className='flex h-30 gap-3 '>
-            <img src={CampaImg[0]}  className='border-2 border-gray-500 cursor-pointer'/>
-            <img src={CampaImg[1]} className='border-2 border-gray-500 cursor-pointer'/>
-            <img src={CampaImg[2]} className='border-2 border-gray-500 cursor-pointer'/>
-            <img src={CampaImg[3]} className='border-2 border-gray-500 cursor-pointer'/>
-            <img src={CampaImg[4]} className='border-2 border-gray-500 cursor-pointer'/>
+            <img src={CampaImg[0]} onClick={() => changeImg(CampaImg[0])} className='border-2 border-gray-500 cursor-pointer'/>
+            <img src={CampaImg[1]} onClick={() => changeImg(CampaImg[1])} className='border-2 border-gray-500 cursor-pointer'/>
+            <img src={CampaImg[2]} onClick={() => changeImg(CampaImg[2])} className='border-2 border-gray-500 cursor-pointer'/>
+            <img src={CampaImg[3]} onClick={() => changeImg(CampaImg[3])} className='border-2 border-gray-500 cursor-pointer'/>
+            <img src={CampaImg[4]} onClick={() => changeImg(CampaImg[4])} className='border-2 border-gray-500 cursor-pointer'/>
           </div>
       </div>
     </div>
-
+    
   )
 }
 
