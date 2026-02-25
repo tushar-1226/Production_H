5679f0c4c7e217ee0ce59e4de39c18b3fe38e8aa
harshit chakrawal <chakrawalharshit@gmail.com>
2026-02-26 01:50:32 +0530

    added usemodel

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const drinkRoute = require('./routes/drinks.route')
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/drink", drinkRoute )
 
---
+++ server/src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel;
