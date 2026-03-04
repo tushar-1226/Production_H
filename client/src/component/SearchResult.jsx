3eb69c495cba97359da538beea04cd3e6e1590d9
harshit chakrawal <chakrawalharshit@gmail.com>
2026-03-04 17:56:15 +0530

    resused drink card

--- client/src/component/SearchResult.jsx
+++ client/src/component/SearchResult.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import axios from "../api/axios";
 import { useState, useEffect } from "react";
+import ShopNavbar from "./ShopNavbar";
+import DrinkCard from "./DrinkCard";
 
-const SearchResult = () => {
+const SearchResult = ({addToCart}) => {
 
   const { id } = useParams();
 
@@ -22,74 +24,26 @@ const SearchResult = () => {
 
     fetchDrink();
 
-  }, [id]); return (
-    <div className="w-full min-h-screen p-10">
-      <h1 className="text-2xl font-semibold mb-6">
-        Search Result
-      </h1>
+  }, [id]);
+  return (
+    <div className="">
+      <ShopNavbar />
 
-      {drink && (
-  <div className="w-[280px] bg-white flex-shrink-0 rounded-3xl shadow-lg p-4 hover:shadow-2xl transition flex flex-col">
+      <div className="w-full min-h-screen p-10">
+        <h1 className="text-2xl font-semibold underline underline-offset-4 mb-4 mt-10 pl-2">
+          Search Result
+        </h1>
 
-    <div className="relative">
-      <img
-        src={drink.image}
-        alt={drink.name}
-        className="w-full h-[220px] object-contain rounded-2xl"
-      />
 
-      <span className="absolute top-1 left-1 bg-black/60 text-white text-xs px-3 py-1 rounded-full">
-        20% off
-      </span>
-    </div>
+        {drink && (
+          <DrinkCard
+            drink={drink}
+            addToCart={addToCart}
+          />
+        )}
 
-    <div className="mt-4 flex flex-col justify-between flex-grow">
-
-      <div className="flex flex-col">
-
-        <div className="flex justify-between items-center w-full">
-          <h2 className="text-lg font-semibold">
-            {drink.name}
-          </h2>
-
-          <span className="bg-gray-800 text-white px-3 py-1 text-sm rounded-full">
-            ₹{drink.price}
-          </span>
-        </div>
-
-        <p className="hidden md:block text-gray-500 text-sm mt-2 line-clamp-3">
-          {drink.description}
-        </p>
 
       </div>
-
-      <div>
-        <div className="flex items-center gap-2 mt-1 pt-4 whitespace-nowrap">
-          <span className="bg-gray-100 px-3 py-1 text-xs rounded-full flex items-center">
-            ⭐ {drink.rating}
-          </span>
-
-          <span className="bg-gray-100 px-3 py-1 text-xs rounded-full flex items-center">
-            {drink.isAvailable ? "Available" : "Out of stock"}
-          </span>
-        </div>
-
-        <div className="mt-auto">
-          <button
-            onClick={() => addToCart(drink)}
-            className="w-full mt-5 bg-[#385170]/70 text-white py-3 rounded-full font-medium hover:bg-[#385170] transition-all duration-300 hover:scale-105"
-          >
-            Add to cart
-          </button>
-        </div>
-      </div>
-
-    </div>
-
-  </div>
-)}
-      
-
     </div>
   );
 };
--- client/src/component/Trending.jsx
+++ client/src/component/Trending.jsx
@@ -3,7 +3,7 @@ import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { CartContext } from "../context/CartContext";
 import Loader from "./Loader";
 import axios from "../api/axios";
-
+import DrinkCard from "../component/DrinkCard";
 
 const Trending = () => {
 
@@ -107,64 +107,15 @@ const Trending = () => {
         )}
         <div ref={scrollRef}
           onScroll={checkScrollPosition} className="flex  overflow-x-auto gap-6 scroll-smooth scrollbar-hide">
+
           {trending.map((drink) => (
-            <div
+            <DrinkCard
               key={drink._id}
-              className="w-[280px] bg-white flex-shrink-0 rounded-3xl shadow-lg p-4 hover:shadow-2xl transition flex flex-col ">
-
-              <div className="relative">
-                <img
-                  src={drink.image}
-                  alt={drink.name}
-                  className="w-full h-[220px] object-contain rounded-2xl"
-                />
-
-                <span className="absolute top-1 left-1 bg-black/60 text-white text-xs px-3 py-1 rounded-full">
-                  20% off
-                </span>
-              </div>
-
-              <div className="mt-4 flex flex-col justify-between flex-grow">
-
-                <div className="flex flex-col">
-
-                  <div className="flex justify-between items-center w-full">
-                    <h2 className="text-lg font-semibold">
-                      {drink.name}
-                    </h2>
-
-                    <span className="bg-gray-800 text-white px-3 py-1 text-sm rounded-full">
-                      ₹{drink.price}
-                    </span>
-                  </div>
-
-                  <p className="hidden md:block text-gray-500 text-sm mt-2 line-clamp-3">
-                    {drink.description}
-                  </p>
-
-                </div>
-
-                <div className="">
-                  <div className="flex items-center gap-2 mt-1  pt-4 whitespace-nowrap">
-                    <span className="bg-gray-100 px-3 py-1 text-xs rounded-full flex items-center">
-                      ⭐ {drink.rating}
-                    </span>
-
-                    <span className="bg-gray-100 px-3 py-1 text-xs rounded-full flex items-center">
-                      {drink.isAvailable ? "Available" : "Out of stock"}
-                    </span>
-                  </div>
-
-                  <div className="mt-auto">
-                    <button onClick={() => addToCart(drink)} className="w-full mt-5 bg-[#385170]/70 text-white py-3 rounded-full font-medium hover:bg-[#385170] transition-all duration-300 hover:scale-105">
-                      Add to cart
-                    </button>
-                  </div>
-                </div>
-
-              </div>
-            </div>
+              drink={drink}
+              addToCart={handleAddToCart}
+            />
           ))}
+
         </div>
         {showRightArrow && (
           <ChevronRight
