40088ed6a41febdc8da5c71091f59ff6f68441a3
harshit chakrawal <chakrawalharshit@gmail.com>
2026-03-10 09:32:13 +0530

    updated drinkRoute

--- server/src/routes/drinks.route.js
+++ server/src/routes/drinks.route.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const router = express.Router()
 
+const Drink = require("../models/drinks.model")
+
 const { createDrinks, fetchDrinks, updateDrinks, deleteDrinks } = require("../controllers/drink.controller")
 
 router.post("/", createDrinks)
+
 router.get("/", fetchDrinks)
 
 router.delete("/:id", deleteDrinks)
+
 router.patch("/:id", updateDrinks)
+
 
 router.get("/search", async (req, res) => {
   try {
+
     const searchTerm = req.query.query
 
     if (!searchTerm) {
@@ -33,6 +39,7 @@ router.get("/search", async (req, res) => {
   }
 })
 
+
 router.get("/:id", async (req, res) => {
   try {
 
@@ -48,5 +55,6 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ message: "Server error" })
   }
 })
+
 
 module.exports = router
